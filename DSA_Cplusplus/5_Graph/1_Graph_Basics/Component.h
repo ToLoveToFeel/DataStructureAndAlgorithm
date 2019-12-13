5ce3176067bc638435664602be94612a42c5172d
i-love-linux <1137247975@qq.com>
2019-12-13 11:00:22 +0800

    Graph

--- DSA_Cplusplus/5_Graph/1_Graph_Basics/Component.h
+++ DSA_Cplusplus/5_Graph/1_Graph_Basics/Component.h
@@ -1,1 +1,65 @@
 #pragma once
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+// 图的深度遍历
+template <typename Graph>
+class Component {
+private:
+	Graph& G;
+	bool* visited;	// 记录该节点是否访问过
+	int ccount = 0;	// 记录图中连通分量的个数
+	int* id;		// 并查集，记录节点是否属于同一个连通分量
+
+	// 图的深度遍历，递归算法
+	void dfs(int v) {
+		visited[v] = true;
+		id[v] = ccount;
+		typename Graph::adjIterator adj(G, v);
+		for (int i = adj.begin(); !adj.end(); i = adj.next())
+			if (!visited[i])
+				dfs(i);
+	}
+
+public:
+	// 构造函数
+	Component(Graph& graph) :G(graph) {
+		visited = new bool[G.V()];
+		id = new int[G.V()];
+		for (int i = 0; i < G.V(); i++)
+		{
+			visited[i] = false;
+			id[i] = -1;
+		}
+		ccount = 0;
+
+		for(int i = 0; i < G.V(); i++)
+			if (!visited[i]) {
+				dfs(i);
+				ccount++;
+			}
+				
+	}
+
+	// 析构函数
+	~Component() {
+		delete[] visited;
+		delete[] id;
+	}
+
+	// 返回连通分量的个数
+	int count() {
+		return ccount;
+	}
+
+	// 判断两个节点是否属于同一个连通分量
+	bool isConnected(int v, int w) {
+		assert(v >= 0 && v < G.V());
+		assert(w >= 0 && w < G.V());
+		assert(id[v] != -1 && id[w] != -1);
+
+		return id[v] == id[w];
+	}
+
+};
--- DSA_Cplusplus/5_Graph/1_Graph_Basics/main.cpp
+++ DSA_Cplusplus/5_Graph/1_Graph_Basics/main.cpp
@@ -1,19 +1,49 @@
 #include "ReadGraph.h"
 #include "DenseGraph.h"
 #include "SparseGraph.h"
+#include "Component.h"
+#include "Path.h"
+#include "ShortestPath.h"
 
 int main(void) {
-	string filename = "testG2.txt";
-	DenseGraph g = DenseGraph(7, false);
-	ReadGraph<DenseGraph> readGraph(g, filename);
-	g.show();
+
+	// TestG1.txt
+	// DenseGraph
+	// 稠密图 - 邻接矩阵
+	string filename1 = "testG1.txt";
+	DenseGraph g1 = DenseGraph(13, false);
+	ReadGraph<DenseGraph> readGraph1(g1, filename1);
+	Component<DenseGraph> component1(g1);
+	cout << "TestG1.txt, Component Count: " << component1.count() << endl;
+	//g1.show();
+
+
+	// TestG2.txt
+	// SparseGraph
+	// 稀疏图 - 邻接表
+	string filename2 = "testG2.txt";
+	SparseGraph g2 = SparseGraph(7, false);
+	ReadGraph<SparseGraph> readGraph2(g2, filename2);
+	Component<SparseGraph> component2(g2);
+	cout << "TestG2.txt, Component Count: " << component2.count() << endl;
+	//g2.show();
 
 	cout << endl;
 
-	SparseGraph g2 = SparseGraph(7, false);
-	ReadGraph<SparseGraph> readGraph2(g2, filename);
-	g2.show();
 	
+	// 测试深度遍历
+	Component<DenseGraph> component(g1);
+	cout << "Test Random Graph, Component Count: " << component.count() << endl;
+
+	// 测试寻路算法
+	Path<SparseGraph> dfs(g2, 0);
+	cout << "DFS : ";
+	dfs.showPath(6);
+
+	// 测试广度遍历，无权图最短路径
+	ShortestPath< SparseGraph> bfs(g2, 0);
+	cout << "BFS : ";
+	bfs.showPath(6);
 
 	return 0;
 }
