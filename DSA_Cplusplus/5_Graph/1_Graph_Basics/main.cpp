a96f771b57b8b1c7f3837eb9fa4382f42401a75d
i-love-linux <1137247975@qq.com>
2019-12-13 09:57:42 +0800

    SparseGraph

--- DSA_Cplusplus/5_Graph/1_Graph_Basics/main.cpp
+++ DSA_Cplusplus/5_Graph/1_Graph_Basics/main.cpp
@@ -1,11 +1,19 @@
 #include "ReadGraph.h"
 #include "DenseGraph.h"
+#include "SparseGraph.h"
 
 int main(void) {
 	string filename = "testG2.txt";
 	DenseGraph g = DenseGraph(7, false);
 	ReadGraph<DenseGraph> readGraph(g, filename);
 	g.show();
+
+	cout << endl;
+
+	SparseGraph g2 = SparseGraph(7, false);
+	ReadGraph<SparseGraph> readGraph2(g2, filename);
+	g2.show();
+	
 
 	return 0;
 }
