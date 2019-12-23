3ddd03fe3f01d845d0089d1ab9849dd73a4d6386
i-love-linux <1137247975@qq.com>
2019-12-23 08:55:09 +0800

    MergeSortTDVisualization

--- DSA_VisualizationAlgorithm/4_Sort_Visualization/Merge_Sort_TD_Visualization/src/AlgoVisualizer.java
+++ DSA_VisualizationAlgorithm/4_Sort_Visualization/Merge_Sort_TD_Visualization/src/AlgoVisualizer.java
@@ -15,7 +15,7 @@ public class AlgoVisualizer {  // 控制层
 
         // 初始化视图
         EventQueue.invokeLater(()->{  // GUI创建的代码放到了新的线程(事件分发线程，事件派生队列)中
-            frame = new AlgoFrame("Welcome", sceneWidth, sceneHeight);
+            frame = new AlgoFrame("Merge Sort TD Visualization", sceneWidth, sceneHeight);
 
             new Thread(() -> {
                 run();
